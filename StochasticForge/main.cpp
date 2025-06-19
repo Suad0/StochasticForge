e732924655fb09d519e6b155b0e102f1d8ac5f96
suad <suad.demiri@it.htlhl.at>
2025-06-19 02:00:26 +0200

    Initial Commit

---
+++ StochasticForge/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StochasticForge
+//
+//  Created by Suad Demiri on 19.06.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
